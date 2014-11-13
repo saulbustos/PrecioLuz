c760db249dd65af648ddf87aeb4f880f62e1d2f8
saulbustos <saul.bustos@telefonica.net>
2014-11-13 16:07:14 +0100

    Delete bins

--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Administrador\Documents\NetBeansProjects\PrecioLuz-V2.6-trastoskWh\bin\AndroidManifest.xml : \
-C:\Users\Administrador\Documents\NetBeansProjects\PrecioLuz-V2.6-trastoskWh\AndroidManifest.xml \
--- bin/PrecioLuz-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\Users\Administrador\Documents\NetBeansProjects\PrecioLuz-V2.6-trastoskWh\bin\PrecioLuz-debug-unaligned.apk : \
-C:\Users\Administrador\Documents\NetBeansProjects\PrecioLuz-V2.6-trastoskWh\bin\PrecioLuz.ap_ \
-C:\Users\Administrador\Documents\NetBeansProjects\PrecioLuz-V2.6-trastoskWh\bin\classes.dex \
-C:\Users\Administrador\Desktop\adt-bundle-windows-x86\adt-bundle-windows-x86\sdk\tools\support\annotations.jar \
