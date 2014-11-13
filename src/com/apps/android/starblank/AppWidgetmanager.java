0c1fef65e0356d4e08d8e6a68cf0ad61f18ef074
saulbustos <saul.bustos@telefonica.net>
2014-11-13 15:55:49 +0100

    Update AppWidgetmanager.java

--- src/com/apps/android/starblank/AppWidgetmanager.java
+++ src/com/apps/android/starblank/AppWidgetmanager.java
@@ -1,5 +1,7 @@
 package com.apps.android.starblank;
 
+//dummy class hecha adrede para compatibilidad con algunos androids que 
+//se resisten a mostrar el widget
 public class AppWidgetmanager {
 
     
